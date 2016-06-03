cfdaac573e909e9644507ece9b6681c90bd3fa49
Sam <sam.king@workstar.com.au>
2016-06-03 11:28:40 +1000

    worked with my own code

--- ratings.js
+++ ratings.js
@@ -1,15 +1,23 @@
 var ages = [1, 23, 8, 12, 16];
 var ratings = ['G', 'PG', 'M', 'MA'];
 var minAgeForRating = [0, 9, 12, 15];
-
+/*sam's first solution*/
 /*ratings.forEach(function(item, index){
 	var leftAges=ages.filter(function(minAge){
 		return minAge>=minAgeForRating[index];
 	}).toString();
 	console.log(ratings[index]+ '\n' +leftAges);
 });*/
-
-var ratingsAllowed=ages.map(function(age){
+/*sam's second solution*/
+ages.forEach(function(age, index){
+  var leftRatings= ratings.filter(function(rating, index){
+	  return age>=minAgeForRating[index];
+  });  
+	console.log("Age "+age +" is allowed to see: "+leftRatings.toString());
+	  
+});
+/*jess's solution*/
+/*var ratingsAllowed=ages.map(function(age){
 	return ratings.filter(function(rating, index){
 		return age>= minAgeForRating[index];
 	});
@@ -17,32 +25,20 @@ var ratingsAllowed=ages.map(function(age){
 
 ages.forEach(function(age, index){
 	console.log("Age "+age+" is allowed to se: "+ratingsAllowed[index].toString());
-});
+});*/
 
 
+
+/*half of martin's solution*/
 /*var allowed=ages.forEach(function(num){
 	function ratingsFilter(value,i){
 		return num>minAgeForRating[i];
 	}
 })
 */
+/*var allowed=ages.forEach(function(age){
+	function ratingsFilter(rating,index){
+		return age>minAgeForRating[index];
+	}
+})*/
 
-/*ages.forEach(function(item, index){
-
-  var leftMinAgeForRating= minAgeForRating.filter(function(itemInminAgeForRating){
-	  return ages[index]>=itemInminAgeForRating;
-	  
-
-  });
-  
-
-*/
- /*  leftMinAgeForRating.forEach(function(item, index){
-	  var newArray=ratings.map(function(itemInRatings){
-		  return itemInRatings[index];
-	  });
-	  console.log(newArray);
-   });*/
-	//console.log("Age "+ages[index] +" is allowed to see: "+leftMinAgeForRating);
-	  
-//});
